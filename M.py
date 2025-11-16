b488d337f120f62816ae550be42a068c96166939
Deepanjali Pathak <deepanjalipathak2005@gmail.com>
2025-11-16 23:43:41 +0530

    updating

---
+++ M.py
@@ -0,0 +1,11 @@
+c = input()
+
+if '0' <= c <= '9':
+    print("IS DIGIT")
+elif 'A' <= c <= 'Z':
+    print("ALPHA")
+    print("IS CAPITAL")
+elif 'a' <= c <= 'z':
+    print("ALPHA")
+    print("IS SMALL")
+    
