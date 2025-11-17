cca586cb538f664bf2f943582d3338146f2a2adc
Deepanjali Pathak <deepanjalipathak2005@gmail.com>
2025-11-17 22:03:32 +0530

    commiting

---
+++ O.py
@@ -0,0 +1,14 @@
+A,S,B=input().split()
+A=int(A)
+B=int(B)
+match S:
+    case '+':
+        print(A+B)
+    case '-':
+        print(A-B)
+    case '*':
+        print(A*B)
+    case '/':
+        print(A/B)
+    
+
