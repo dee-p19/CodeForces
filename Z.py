00abfaa7f7f315677af92446c6911234d4c722ab
Deepanjali Pathak <deepanjalipathak2005@gmail.com>
2025-11-20 22:04:48 +0530

    updating

---
+++ Z.py
@@ -0,0 +1,17 @@
+# A,B,C,D=map(int,input().split())
+# if A**B>C**D:
+#     print("YES")
+# else:
+#     print("NO")
+
+import math
+
+A, B, C, D = map(int, input().split())
+
+left = B * math.log(A)
+right = D * math.log(C)
+
+if left > right:
+    print("YES")
+else:
+    print("NO")
