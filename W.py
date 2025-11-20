65150511fd100c0d79b8885ffd3befe8538c4568
Deepanjali Pathak <deepanjalipathak2005@gmail.com>
2025-11-20 22:03:00 +0530

    updating

---
+++ W.py
@@ -0,0 +1,19 @@
+A,S,B,Q,C=input().split()
+A=int(A)
+B=int(B)
+C=int(C)
+if(S=='+'):
+   if(A+B==C):
+    print("Yes")
+   else:
+    print(A+B)
+if(S=='-'):
+   if(A-B==C):
+    print("Yes")
+   else:
+    print(A-B)
+if(S=='*'):
+   if(A*B==C):
+    print("Yes")
+   else:
+    print(A*B)
