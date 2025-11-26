c543e36162f976c44a12e2ca51f28d076d8f2543
Deepanjali Pathak <deepanjalipathak2005@gmail.com>
2025-11-26 08:48:47 +0530

    updating

---
+++ sheet2/B.py
@@ -0,0 +1,14 @@
+N=int(input())
+for i in range(2,N+1,2):
+ print(i)
+if N<2:
+ print(-1)
+
+# n=int(input())
+# fount=False
+# for i in range(1,N+1):
+#   if i%2==0:
+#     print(i)
+#     fount=True
+# if not fount:
+#   print(-1)
