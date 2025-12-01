cbd1c01b02c95cb1f13b23efa40f52a7f3f24372
Deepanjali Pathak <deepanjalipathak2005@gmail.com>
2025-12-01 18:43:14 +0530

    updating

---
+++ sheet2/T.py
@@ -0,0 +1,11 @@
+N=int(input())
+a=0
+for i in range(N):
+    # if(i>=N):
+    #   a-=1
+    # if(i<N):
+       print(" "*(N-i-1)+"*"*(1+2*a))
+    # else:
+    #    print(" "*(N-i-(N-i)-(N-i))+"*"*(1+2*a))
+    # if(i<N):
+      
