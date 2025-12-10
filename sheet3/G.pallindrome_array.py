22373e7f36aad7f4ccc3bc93157e9096b79b5ac4
Deepanjali Pathak <deepanjalipathak2005@gmail.com>
2025-12-10 18:45:30 +0530

    updating

--- sheet3/G.pallindrome_array.py
+++ sheet3/G.pallindrome_array.py
@@ -1,9 +1,11 @@
 N=int(input())
 lst=list(map(int,input().split()))
-for i in range(N//2):
-    if(lst[i]!=lst[-i-1]):
-     print("NO")
-     break
-if(i==N//2):
+lst2=[]
+for i in range(N):
+    lst2.append(lst[-1-i])
+
+if(lst==lst2):
    print("YES")
+else:
+    print("NO")
    
