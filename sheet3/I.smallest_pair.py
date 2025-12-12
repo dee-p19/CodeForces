ff6c63c1c7bae98b7d6f1631d0e21de30afa0344
Deepanjali Pathak <deepanjalipathak2005@gmail.com>
2025-12-12 22:24:51 +0530

    update

--- sheet3/I.smallest_pair.py
+++ sheet3/I.smallest_pair.py
@@ -1,23 +1,37 @@
-T=int(input())
-lst=[]
-for i in range(T):
-    N=int(input())
-    lst.append(list(map(int,input().split())))
+# T=int(input())
+# lst=[]
+# for i in range(T):
+#     N=int(input())
+#     lst.append(list(map(int,input().split())))
+# for i in lst:
+#     mini=10**6
+#     for j in range(N):
+#         for k in range(j+1,len(i)):
+#            sum=0
+#            sum=j+i[k]+(kin+1)-jin
+#            if(mini>sum):
+#              mini=sum
+#            kin+=1
+#         jin+=1
+#     print(mini)
+#     print(" ") 
 
 
-for i in lst:
-    mini=10**6
-    jin=1
-    for j in i:
-        kin=jin
-        for k in range(jin,len(i)):
-           sum=0
-           sum=j+i[k]+(kin+1)-jin
-           if(mini>sum):
-             mini=sum
-           kin+=1
-        jin+=1
+T = int(input())
+
+for _ in range(T):
+    N = int(input())
+    A = list(map(int, input().split()))
+
+    mini = float('inf')
+
+    for i in range(N):
+        for j in range(i+1, N):
+            value = A[i] + A[j] + (j - i)
+            if value < mini:
+                mini = value
+
     print(mini)
-    print(" ")  
+
 
 
