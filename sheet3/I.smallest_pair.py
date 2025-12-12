90a5fadffa528ccc3c98d835a940a7aa0891ebed
Deepanjali Pathak <deepanjalipathak2005@gmail.com>
2025-12-12 22:31:31 +0530

    updating
    Refactor the smallest pair calculation for clarity and efficiency.

--- sheet3/I.smallest_pair.py
+++ sheet3/I.smallest_pair.py
@@ -1,22 +1,18 @@
-T=int(input())
-lst=[]
-for i in range(T):
-    N=int(input())
-    lst.append(list(map(int,input().split())))
+T = int(input())
 
-mini=10**6
-for i in lst:
-    jin=1
-    for j in i:
-        kin=jin
-        for k in range(jin,len(i)):
-           sum=0
-           sum=j+i[k]+(kin+1)-jin
-           if(mini>sum):
-             mini=sum
-           print(f"{j}+{i[k]}+{kin+1}-{jin}={sum}")
-           kin+=1
-        jin+=1
-    print(mini)  
+for _ in range(T):
+    N = int(input())
+    A = list(map(int, input().split()))
+
+    mini = float('inf')
+
+    for i in range(N):
+        for j in range(i+1, N):
+            value = A[i] + A[j] + (j - i)
+            if value < mini:
+                mini = value
+
+    print(mini)
+
 
 
