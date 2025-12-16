18a09c7d3b0156ae90dd694707018f718cc0f6a2
Deepanjali Pathak <deepanjalipathak2005@gmail.com>
2025-12-17 01:57:04 +0530

    commiting

---
+++ sheet3/O.fabonacci.py
@@ -0,0 +1,30 @@
+# def fib(N):
+#   if(N==1):
+#     fibo=0
+#   elif(N==2):
+#     fibo=1
+#   else:
+#     fibo= fib(N-1)+ fib(N-2)
+#   return fibo
+
+# N=int(input())
+# print(fib(N))
+
+def fib(N):
+    if N <= 0:
+        return 0
+    elif N == 1:
+        return 0
+    elif N == 2:
+        return 1
+    
+    a, b = 0, 1
+    for _ in range(3, N + 1):
+        # Calculate the next number
+        a, b = b, a + b
+    
+    # b now holds the N-th Fibonacci number
+    return b
+
+N = int(input())
+print(fib(N))
---
+++ sheet3/P.minimum_num.py
@@ -0,0 +1,15 @@
+N=int(input())
+lst=list(map(int,input().split()))
+c=True
+count=0
+while(c==True):
+    for i in lst:
+        if(i%2!=0):
+            c=False
+            break
+    if(c==True):
+        for i in range(N):
+            lst[i]//=2
+        count+=1
+           
+print(count)
