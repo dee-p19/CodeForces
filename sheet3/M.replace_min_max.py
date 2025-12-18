ce5675bdbdebd17db31e11349500fb50188c9749
Deepanjali Pathak <deepanjalipathak2005@gmail.com>
2025-12-18 22:02:21 +0530

    commmiting

--- sheet3/M.replace_min_max.py
+++ sheet3/M.replace_min_max.py
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 N=int(input())
 lst=list(map(int,input().split()))
 maxi=max(lst)
@@ -8,14 +8,3 @@ b=lst.index(mini)
 lst[a],lst[b]=lst[b],lst[a]
 for i in lst:
     print(i,end=" ")
-=======
-N=int(input())
-lst=list(map(int,input().split()))
-maxi=max(lst)
-mini=min(lst)
-a=lst.index(maxi)
-b=lst.index(mini)
-lst[a],lst[b]=lst[b],lst[a]
-for i in lst:
-    print(i,end=" ")
->>>>>>> 464430a6b99620935fd179a9de751edc907c96ca
