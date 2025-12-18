10321980f4252b9d3506fceb9ee55f892ca82e5d
Deepanjali Pathak <deepanjalipathak2005@gmail.com>
2025-12-18 22:00:50 +0530

    update

--- sheet3/P.minimum_num.py
+++ sheet3/P.minimum_num.py
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 N=int(input())
 lst=list(map(int,input().split()))
 c=True
@@ -14,20 +14,3 @@ while(c==True):
         count+=1
            
 print(count)
-=======
-N=int(input())
-lst=list(map(int,input().split()))
-c=True
-count=0
-while(c==True):
-    for i in lst:
-        if(i%2!=0):
-            c=False
-            break
-    if(c==True):
-        for i in range(N):
-            lst[i]//=2
-        count+=1
-           
-print(count)
->>>>>>> 464430a6b99620935fd179a9de751edc907c96ca
