a9f7920d9da4ea88f1bc73412190a807c5927516
Deepanjali Pathak <deepanjalipathak2005@gmail.com>
2025-12-20 20:23:53 +0530

    changing

--- sheet3/R.permutation_witharray.py
+++ sheet3/R.permutation_witharray.py
@@ -1,12 +1,8 @@
-N=int(input())
-A=list(map(int,input().split()))
-B=list(map(int,input().split()))
-check=True
-for i in range(N):
-    if(B[i]!=A[i]):
-     check=False
-     break
-if(check):
-   print("yes")
+n = int(input())
+A = list(map(int, input().split()))
+B = list(map(int, input().split()))
+
+if sorted(A) == sorted(B):
+    print("yes")
 else:
-   print("no")
+    print("no")
