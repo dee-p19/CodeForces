c7f247ac17a04e2d4dd6498d5d3fe636d9b552d4
Deepanjali Pathak <deepanjalipathak2005@gmail.com>
2025-12-22 17:51:27 +0530

    commiting

--- sheet3/V.frequency_array.py
+++ sheet3/V.frequency_array.py
@@ -3,3 +3,9 @@ A=list(map(int,input().split()))
 for i in range(1,M+1):
     freq=A.count(i)
     print(freq)
+
+N,M=map(int,input().split())
+A=list(map(int,input().split()))
+for i in range(1,M+1):
+    freq=A.count(i)
+    print(freq)
