f564bce3736adf903466e4507e283a9d42014e6a
Deepanjali Pathak <deepanjalipathak2005@gmail.com>
2025-12-22 18:36:12 +0530

    Add files via upload

---
+++ sheet3/U.is_B_asubsequence_of_A.py
@@ -0,0 +1,15 @@
+N,M=map(int,(input()).split())
+A=list(map(int,input().split()))
+B=list(map(int,input().split()))
+j=0
+for i in range(N):
+   if(A[i]==B[j]):
+      j+=1
+   if(j==M):
+      break
+if(j==M):
+   print("YES")
+else:
+   print("NO")
+
+
