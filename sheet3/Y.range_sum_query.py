d18e7d720fd2841225219cb642287f70190664bd
Deepanjali Pathak <deepanjalipathak2005@gmail.com>
2025-12-25 20:53:17 +0530

    commit

--- sheet3/Y.range_sum_query.py
+++ sheet3/Y.range_sum_query.py
@@ -1,12 +1,24 @@
-N,Q=map(int,input().split())
-lst=list(map(int,input().split()))
-p=[]
-for i in range(Q):
-  p.append(list(map(int,input().split())))
-for i in p:
-  sum=0
-  for j in range(i[0]-1,i[1]):
-     sum+=lst[j]
-  print(sum)
+# Read N and Q
+N, Q = map(int, input().split())
+
+# Read array elements
+A = list(map(int, input().split()))
+
+# Build prefix sum array
+prefix = [0] * (N + 1)
+for i in range(N):
+    prefix[i + 1] = prefix[i] + A[i]
+
+# Store results instead of printing immediately
+results = []
+
+# Read all queries
+for _ in range(Q):
+    L, R = map(int, input().split())
+    results.append(prefix[R] - prefix[L - 1])
+
+# Print output after all input is processed
+for ans in results:
+    print(ans)
 
   
