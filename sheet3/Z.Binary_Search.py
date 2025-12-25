e60d34656061b1ec50f008c8d675dc9347cecddc
Deepanjali Pathak <deepanjalipathak2005@gmail.com>
2025-12-25 20:54:05 +0530

    commit

--- sheet3/Z.Binary_Search.py
+++ sheet3/Z.Binary_Search.py
@@ -2,9 +2,10 @@ N,Q=map(int,input().split())
 lst=list(map(int,input().split()))
 a=[]
 for i in range(Q):
-    a.append(int(input()))
+   nput=int(input())
+   if (nput in lst):
+       a.append("found")
+   else:
+       a.append("not found")
 for i in a:
-    if(i in lst):
-        print("found")
-    else:
-        print("not found")
+    print(i)
