c15cad2610bc14d71a7f89cf1a05fae83817c85c
Deepanjali Pathak <deepanjalipathak2005@gmail.com>
2025-12-29 16:59:51 +0530

    commit

--- sheet3/U.is_B_asubsequence_of_A.py
+++ sheet3/U.is_B_asubsequence_of_A.py
@@ -13,3 +13,19 @@ else:
    print("NO")
 
 
+# N,M=map(int,(input()).split())
+# A=list(map(int,input().split()))
+# B=list(map(int,input().split()))
+# j=0
+# for i in range(N):
+#    if(A[i]==B[j]):
+#       j+=1
+#    if(j==M):
+#       break
+# if(j==M):
+#    print("YES")
+# else:
+#    print("NO")
+
+
+
