95c492ca72838ddd45c6706c423c418bb7298353
Deepanjali Pathak <deepanjalipathak2005@gmail.com>
2025-12-30 23:20:48 +0530

    changing code

--- sheet3/V.frequency_array.py
+++ sheet3/V.frequency_array.py
@@ -1,5 +1,11 @@
-N,M=map(int,input().split())
-A=list(map(int,input().split()))
-for i in range(1,M+1):
-    freq=A.count(i)
-    print(freq)
+n, m = map(int, input().split())
+arr = list(map(int, input().split()))
+
+freq = [0] * (m + 1)
+
+for x in arr:
+    freq[x] += 1
+
+for i in range(1, m + 1):
+    print(freq[i])
+
