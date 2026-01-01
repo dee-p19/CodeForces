2bf490825d76d5596c9288e45f0bf938aaf918f4
Deepanjali Pathak <deepanjalipathak2005@gmail.com>
2026-01-01 22:53:10 +0530

    commit change

--- sheet3/O.fabonacci.py
+++ sheet3/O.fabonacci.py
@@ -23,22 +23,9 @@ def fib(N):
     for _ in range(3, N + 1):
         # Calculate the next number
         a, b = b, a + b
-    
+
     # b now holds the N-th Fibonacci number
     return b
 
 N = int(input())
 print(fib(N))
-=======
-def fib(N):
-  if(N==1):
-    fibo=0
-  elif(N==2):
-    fibo=1
-  else:
-    fibo= fib(N-1)+ fib(N-2)
-  return fibo
-
-N=int(input())
-print(fib(N))
->>>>>>> 464430a6b99620935fd179a9de751edc907c96ca
