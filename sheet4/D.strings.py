42033dbeefa0273ed96fb53079630b1eacbfab10
Deepanjali Pathak <deepanjalipathak2005@gmail.com>
2026-01-08 12:02:29 +0530

    commit

--- sheet4/D.strings.py
+++ sheet4/D.strings.py
@@ -1,7 +1,13 @@
 A = input()
 B = input()
-print(len(A),len(B))
-print(A+B)
-a = A.replace(A[0] , B[0])
-b = B.replace(B[0] , A[0])
-print(a,b)
+
+print(len(A), len(B))
+print(A + B)
+
+newA = B[0] + A[1:]
+newB = A[0] + B[1:]
+
+# a = A.replace(A[0] , B[0])
+# b = B.replace(B[0] , A[0])
+
+print(newA, newB)
