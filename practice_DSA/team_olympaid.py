be59b0d28d0ee8c5763397bd72b8b2e6950f135a
Deepanjali Pathak <deepanjalipathak2005@gmail.com>
2026-01-21 18:10:00 +0530

    commit

---
+++ practice_DSA/team_olympaid.py
@@ -0,0 +1,17 @@
+n = int(input())
+lst = list(map(int,input().split()))
+c1=[]
+c2=[]
+c3=[]
+for i in range(len(lst)):
+  if(lst[i]==1):
+    c1.append(i)
+  elif lst[i]==2:
+    c2.append(i)
+  else:
+    c3.append(i)
+m = min(len(c1),len(c2),len(c3)) 
+for i in range(m):
+  print(c1[i],c2[i],c3[i])
+  
+  
